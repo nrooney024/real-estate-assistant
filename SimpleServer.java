c3ce66084319f53ba91ba7777d130968ce482bf7
nrooney024 <nrooney024@gmail.com>
2023-12-28 17:51:42 -0500

    Updating server to accept incoming requests to a http server. This will also account for CORS error handling.

--- SimpleServer.java
+++ SimpleServer.java
@@ -1,45 +1,65 @@
+// Import necessary libraries
+import com.sun.net.httpserver.*;
 import java.io.*;
 import java.net.*;
 
-// Class definition for SimpleServer
+// Define the main class
 public class SimpleServer {
-    // The main method - entry point of the application
+    // Define the main method
     public static void main(String[] args) throws IOException {
-        // Define the port number on which the server will listen
+        // Define the port number
         int port = 8080;
-        // Create a ServerSocket that listens on the specified port
-        ServerSocket serverSocket = new ServerSocket(port);
-        // Print to the console that the server has started
+        // Create a new HTTP server on the specified port
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        // Print a message to the console indicating the server has started
         System.out.println("Server started on port: " + port);
 
-        // Infinite loop to keep the server running
-        while (true) {
-            try (
-                // Accept an incoming connection from a client
-                Socket socket = serverSocket.accept();
-                // Create a PrintWriter to send data to the client
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                // Create a BufferedReader to read data from the client
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
-            ) {
-                
-                String inputLine;
-                // Read lines from the client until "null" is received (indicating end of stream)
-                while ((inputLine = in.readLine()) != null) {
-                    // Print the received line to the console
-                    System.out.println("Received: " + inputLine);
-                    // Send an echo response back to the client
-                    out.println("Echo: " + inputLine);
-                    // If the client sends "bye", exit the loop and wait for the next connection
-                    if ("bye".equalsIgnoreCase(inputLine)) {
-                        break;
-                    }
+        // Define a new context for the server
+        server.createContext("/receive-data", new HttpHandler() {
+            // Define the handle method for incoming requests
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                // Check if the request method is OPTIONS
+                if ("OPTIONS".equals(exchange.getRequestMethod())) {
+                    // Set the CORS headers for preflight requests
+                    exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+                    exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "POST");
+                    exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+                    exchange.sendResponseHeaders(204, -1);
+                } else if ("POST".equals(exchange.getRequestMethod())) {
+                    // Set the CORS headers for actual requests
+                    exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+                    exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "POST");
+                    exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+
+                    // Create a new InputStreamReader for the request body
+                    InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
+                    // Create a new BufferedReader for the InputStreamReader
+                    BufferedReader br = new BufferedReader(isr);
+
+                    // Read the first line of the request body
+                    String query = br.readLine();
+                    // Print the received data to the console
+                    System.out.println("Received: " + query);
+
+                    // Define the response string
+                    String response = "Echo: " + query;
+                    // Send the response headers
+                    exchange.sendResponseHeaders(200, response.length());
+                    // Get the response body as an OutputStream
+                    OutputStream os = exchange.getResponseBody();
+                    // Write the response string to the OutputStream
+                    os.write(response.getBytes());
+                    // Close the OutputStream
+                    os.close();
+                } else {
+                    // If the request method is not OPTIONS or POST, send a 405 Method Not Allowed response
+                    exchange.sendResponseHeaders(405, -1);
                 }
-            } catch (IOException e) {
-                // Print an error message if an exception occurs in the try block
-                System.out.println("Exception caught when trying to listen on port " + port + " or listening for a connection");
-                System.out.println(e.getMessage());
             }
-        }
+        });
+
+        // Start the server
+        server.start();
     }
 }
