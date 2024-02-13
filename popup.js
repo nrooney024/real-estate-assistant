b8ab5d7afa3f3a45eecdf36b4b58a9bed2ef8676
nrooney024 <nrooney024@gmail.com>
2024-02-12 22:30:23 -0500

    Changing from scraping to search bar.

--- popup.js
+++ popup.js
@@ -1,53 +1,17 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     // When the popup DOM is fully loaded, retrieve the supermarket data.
-//     chrome.storage.local.get(['supermarkets'], function(result) {
-//       if(result.supermarkets) {
-//         // If supermarkets are found in storage, display them.
-//         console.log("result.supermarkets from popup.js chrome storage: ", result.supermarkets)
-//         displaySupermarkets(result.supermarkets);
-//       }
-//     });
-//   });
-
-  document.getElementById('searchForm').addEventListener('submit', function(event) {
-    console.log("Form submitted...")
-    event.preventDefault(); // Prevent the form from submitting normally
-    const searchQuery = document.getElementById('searchQuery').value;
-  
-    function fetchSupermarkets(searchQuery) {
-      // Use `fetch` to send the address to your API and get the list of supermarkets
-      // Then, store it using chrome.storage or send it directly to your popup
-      
-      console.log("fetchSupermarkets called...")
-      
-      fetch('http://localhost:8080/fetch-address-data', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({ content: searchQuery })
-          })
-          .then(response => response.json())
-          .then(data => {
-              console.log(data)
-              console.log("Successfully received /fetch-address-data response!");
-              console.log("Data from /fetch-address-data: " + data);
-              
-          chrome.storage.local.set({ supermarkets: data }, function() {
-              console.log('Supermarkets are saved in chrome.storage.local');
-          });
-          
-          displaySupermarkets(data)
-      })
-      .catch(error => {
-          console.error('Error fetching supermarket data:', error);
-      });
-    }
-  });
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+  console.log("Form submitted...")
+  event.preventDefault(); // Prevent the form from submitting normally
+  const searchQuery = document.getElementById('searchQuery').value;
+  fetchSupermarkets(searchQuery)
+});
   
 
   
 function displaySupermarkets(supermarkets) {
+    
+    const supermarketsHeader = document.getElementById('supermarkets-header')
+    supermarketsHeader.innerText = 'Supermarkets'
+    
     const displayElement = document.getElementById('display');
 
     // Clear any existing content
@@ -89,4 +53,35 @@ function displaySupermarkets(supermarkets) {
         listItem.appendChild(link);
         list.appendChild(listItem);
     });
+}
+
+
+function fetchSupermarkets(searchQuery) {
+  // Use `fetch` to send the address to your API and get the list of supermarkets
+  // Then, store it using chrome.storage or send it directly to your popup
+  
+  console.log("fetchSupermarkets called...")
+  
+  fetch('http://localhost:8080/fetch-address-data', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ content: searchQuery })
+      })
+      .then(response => response.json())
+      .then(data => {
+          console.log(data)
+          console.log("Successfully received /fetch-address-data response!");
+          console.log("Data from /fetch-address-data: " + data);
+          
+      chrome.storage.local.set({ supermarkets: data }, function() {
+          console.log('Supermarkets are saved in chrome.storage.local');
+      });
+      
+      displaySupermarkets(data)
+  })
+  .catch(error => {
+      console.error('Error fetching supermarket data:', error);
+  });
 }
